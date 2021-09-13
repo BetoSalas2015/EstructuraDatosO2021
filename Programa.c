a03f27ada2cde57e35a242f006c666a30c47a3fc
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2021-09-13 11:15:47 -0500

    Sesión 13 - 13/09/2021

--- Programa.c
+++ Programa.c
@@ -1,9 +1,11 @@
+//  Sesión 13 - 13/09/2021
+
 //  Factorial:   n! = n * n-1 * ... * 3 * 2 * 1
 #include <stdio.h>			// para la entrada y salida
 #include <stdlib.h>			// para el system()
 #include <string.h>			// para el manejo de las cadenas
 
-struct Persona 
+struct Persona				//  DEfine el tipo de dato llamado "Persona"
 {
 	char Nombre[15];
 	char Apellido[15];
@@ -21,26 +23,38 @@ struct Datos				// DEfine un nuevo tipo de dato "Datos"
 int main()
 {
 	int costo = 60;
-	struct Datos Agenda;
+	int posicion;
+	//struct Datos Agenda;		//  DEfino una variable del tipo struct Agenda
+	struct Datos Agenda[10];	//  DEfino un  arreglo de variables del tipo struct Agenda
 
+	//printf("En qué posición deseas se guarden los datos: ");
+	//scanf("%d", &posicion);
+	//fflush(stdin);
 
+	for(posicion = 0; posicion < 2; posicion++)
+	{
+		printf("\nPersona no. %d:\n", posicion);
+		printf("Dame el nombre de la persona: ");
+		gets(Agenda[posicion].nombre.Nombre);
+		fflush(stdin);
+		printf("Dame el apellido de la persona: ");
+		gets(Agenda[posicion].nombre.Apellido);
+		printf("Dame la edad de la persona: ");
+		scanf("%d", &Agenda[posicion].edad);
+		fflush(stdin);				//  Vaciamos el Buffer de teclado parqa forzar se pida el siguiente char
+		printf("Dame el sexo de la persona: ");
+		scanf("%c", &Agenda[posicion].sexo);
+		fflush(stdin);	
+		printf("Dame la dirección de la persona: ");
+		gets(Agenda[posicion].direccion);
 
-	printf("Dame el nombre de la persona: ");
-	gets(Agenda.nombre.Nombre);
-	fflush(stdin);
-	printf("Dame el apellido de la persona: ");
-	gets(Agenda.nombre.Apellido);
-	printf("Dame la edad de la persona: ");
-	scanf("%d", &Agenda.edad);
-	fflush(stdin);				//  Vaciamos el Buffer de teclado parqa forzar se pida el siguiente char
-	printf("Dame el sexo de la persona: ");
-	scanf("%c", &Agenda.sexo);
-	fflush(stdin);	
-	printf("Dame la dirección de la persona: ");
-	gets(Agenda.direccion);
+		
+	} 
 
-	printf("\nNombre: \t%s %s\nEdad: \t\t%d\nSexo:\t\t%c\nVive en \t%s\n", Agenda.nombre.Nombre, Agenda.nombre.Apellido, Agenda.edad, Agenda.sexo, Agenda.direccion);
-	
+	for(posicion = 0; posicion < 2; posicion++)
+	{
+		printf("\nNombre: \t%s %s\nEdad: \t\t%d\nSexo:\t\t%c\nVive en \t%s\n", Agenda[posicion].nombre.Nombre, Agenda[posicion].nombre.Apellido, Agenda[posicion].edad, Agenda[posicion].sexo, Agenda[posicion].direccion);
+	}
 	/*Agenda.edad = 0;
 	Agenda.sexo = '0';
 	strcpy(Agenda.nombre, "0");
