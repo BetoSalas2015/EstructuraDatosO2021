8bb28f9f17c5b8acfedff8ff6d0774c0c27f0327
Roberto Salazar <roberto.salazar@upaep.mx>
2021-10-03 21:28:41 -0500

    Sesión 21 - 29/09/2021

--- Programa.c
+++ Programa.c
@@ -1,35 +1,51 @@
-//  Sesión 17 - 22/09/2021
+//  Sesión 20 - 29/09/2021
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "Estructura.h"
 
-//   Funciones
-float pitagoras(int cateto1, int cateto2)
+int factorialIt(int n)				// Version iterativa de factorial
 {
-	return sqrt( (int)square(cateto1) + square(cateto2) );
+	int  i,fact = 1;
+	for(i = 1; i <= n; i++)
+		fact = fact * i;	
+	return fact;
 }
+
+//   Modelo matemático de factorial
+//   n! = 1						Si n = 1   (cms)
+//   n! = n * (n-1)!			Si n > 1    (aprox)
+int factorial(int n)
+{
+	if(n == 1)				// criterio de paro (cms)
+		return 1;
+	else	
+		return n * factorial(n - 1);	// llamada recursiva (aprox)
+}
+
+//   (a * b) = a				Si b = 1
+//   (a * b) = (a * b-1) + a	Si b > 1
+
+
+//	x^y = 1				Si y = 0
+//  x^y = x * x^y-1		Si y > 0
+
+//  Fi = 1					Si i = 1 o i i = 2
+//  Fi = Fi-1 + Fi-2		Si i > 2
+
+//  Pi = 4					Si i = 1
+//  Pi = Pi-1 - 4/(2i-1)	Si i > 1 y es par
+//  Pi = Pi-1 + 4/(2i-1)	Si i > 1 y es impar
+
 
 int main()
 {
-	datos();
+	int n;
+	printf("Dame el número del que quieres el factorial: ");
+	scanf("%d", &n);
+	
+	printf("El factorial iterativo de %d es: %d\n", n, factorialIt(n) );
+	printf("El factorial recursivo de %d es: %d\n", n, factorial(n) );
+
 	system("pause");
 	return 1;
-}
-
-void datos(void)
-{
-	int c1, c2;
-	float h;
-	printf("Dame el cateto adyacente: ");
-	scanf("%d", &c1);
-	printf("Dame el cateto opuesto: ");
-	scanf("%d", &c2);
-	h = pitagoras(c1, c2);
-	printf("La hipotenusa del triangulo es %f: \n", h);
-}
-
-int square(int numero)
-{
-	return (numero * numero);
 }
