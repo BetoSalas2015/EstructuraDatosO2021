2ac3b76615e9393289ba35131074ad9cce30822e
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2021-10-11 11:29:59 -0500

    Sesión 25 - 11/10/2021

--- Programa.c
+++ Programa.c
@@ -1,30 +1,22 @@
-//  Sesión 23 - 05/10/2021
-#include <math.h>
+//  Sesión 25 - 11/10/2021
+//  Repasdo general de Arreglos
 #include <stdio.h>
 #include <stdlib.h>
 
-void hanoi(int n, char origen, char destino, char aux)
-{
-		//  Programe su algoritmo aquí
-}
 
 int main()
 {
-	int n;
-	char or, dest, aux;
-	printf("Dame el número de discos: ");
-	scanf("%d", &n);
-	fflush(stdin);
-	printf("Dame el poste origen: ");
-	scanf("%c", &or);
-	fflush(stdin);
-	printf("Dame el poste destino: ");
-	scanf("%c", &dest);
-	fflush(stdin);
-	printf("Dame el poste auxiliar: ");
-	scanf("%c", &aux);
+	int y;					// Declaro una variable entera llamada y
+	
+	// Declaro un arreglo de 10 enteros llamado arreglo
+	int arreglo[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}; // Inicialización directa.
+	int arreglo2[10];
+	int matriz[3][3] = { {1,2,3}, {4, 5, 6}, {7, 8,9}};
 
-	hanoi(n, or, dest, aux);
+	for(y = 0; y <= 10; y++)
+		arreglo2[y] = 1;			//  Inicialización manual
+
+	arreglo[8] = 24;
 
 	system("pause");
 	return 1;
