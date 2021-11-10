f20721a008bf2121c35e5cfe383a41b351cc7dd4
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2021-11-10 11:30:46 -0600

    Sesión 37 - 10/11/2021

--- Programa.c
+++ Programa.c
@@ -1,42 +1,41 @@
-//  Sesión 29 - 19/10/2021
+//  Sesión 37 - 09/11/2021
 //  Base.c
 
 #include <stdio.h>		//  Para la I/O o E/S
 #include <stdlib.h>		//  PAra system(), srand() y rand()
-#include <time.h>		//  Para las función de tiempo: time()
 
-// #define macro valor
-#define SIZE 15
-#define RANGO 1000
-
-void imprimeArreglo(int arr[], int n)
+struct datos
 {
-	int i;
-	for(i = 0; i < SIZE; ++i)
-		printf("%d, ", arr[i]);
-	putchar('\n');
-}
-
-//  Implemente su algoritmo de ordenamiento aquí
+	char nombre[30];
+	int edad;
+	char sexo;
+	char direccion[30];
+};
 
 int main()
 {
-	int arreglo[SIZE];				// Creo un archivo de datos de tamaño SIZE
-	int i;							// Variaqble controladora para los ciclos
-	
-	srand( time(0) );					// Proveemos la semilla de numeros aleatorios
+	int costo = 60;			//  Creo una variable entera llamada costo y guardo el valor de 60
+	int *pointer;			//  Creo una variable apuntador a direccciones de m. donde se guardan enteros
+	char *pointer2;
+	struct datos cadena[2];
 
-	for(i = 0; i < SIZE; ++i)
-		arreglo[i] = rand() % RANGO;
+	//  determinar el tamaño de bytes de las cosas en C
+	printf("El tipo de dato char ocupa %d bytes de memoria.\n", sizeof( char ) );
+	printf("El tipo de dato int ocupa %d bytes de memoria.\n", sizeof( int ) );
+	printf("El tipo de dato float ocupa %d bytes de memoria.\n", sizeof( float ) );
+	printf("El tipo de dato double ocupa %d bytes de memoria.\n", sizeof( double ) );
+	printf("El apuntador pointer ocupa %d bytes de memoria.\n", sizeof( pointer ) );
+	printf("El apuntador pointer2 ocupa %d bytes de memoria.\n", sizeof( pointer2 ) );
+	printf("La estructura datos ocupa %d bytes de memoria.\n", sizeof( struct datos ) );
 
-	printf("Arreglo desordenado: \n");
-	imprimeArreglo(arreglo, SIZE);
-
-	//  Invoque su función de Ordenamiento aquí
-
-	printf("Arreglo ordenado: \n");
-	imprimeArreglo(arreglo, SIZE);
+	//  Direcciones de Memoria
+	printf("La dirección de memoria de la variable costo es: %p\n", &costo);		// %p imprime direcciones de memoria
+	printf("La dirección de memoria de la variable pointer es: %p\n", &pointer);
+	printf("La dirección de memoria de la variable pointer2 es: %p\n", &pointer2);
+	printf("La dirección de memoria de la variable cadena[0] es: %p\n", &cadena[0]);
+	printf("La dirección de memoria de la variable cadena[1] es: %p\n", &cadena[1]);
 
 	system("pause");
 	return 1;
 }
+
